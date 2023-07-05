f6044fdad568cd7e77b38087bfcbcc93c3a6925b
David <dbourgon@gmail.com>
2023-07-05 11:39:31 -0400

    updated info

--- ssd1306_font01.c
+++ ssd1306_font01.c
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2022 David Bourgon
+* Copyright (c) 2023 David Bourgon
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
